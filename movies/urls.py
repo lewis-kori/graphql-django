38cd08c30c92e7122af1b86d75bb71adb68fa6d3
lewis <lewiskosh@gmail.com.com>
2019-06-10 11:01:37 +0300

    removed csrf exempt on graphiql view

--- movies/urls.py
+++ movies/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 # from movies.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
